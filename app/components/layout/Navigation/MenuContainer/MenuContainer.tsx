ecee621b779c68640fe0fd4446e77474e963a0f2
Andrey Emelianov <621077@mail.ru>
2023-08-29 19:54:59 +0300

    добавил компонент Menu наполнил его меню айтемами, добавил стили

--- app/components/layout/Navigation/MenuContainer/MenuContainer.tsx
+++ app/components/layout/Navigation/MenuContainer/MenuContainer.tsx
@@ -1,7 +1,17 @@
 import { FC } from 'react';
 
+import Menu from './Menu';
+import GenreMenu from './genres/GenreMenu';
+import { firstMenu, userMenu } from './menu.data';
+
 const MenuContainer: FC = () => {
-	return <div>MenuContainer</div>;
+	return (
+		<div>
+			<Menu menu={firstMenu} />
+			{/* <GenreMenu /> */}
+			<Menu menu={userMenu} />
+		</div>
+	);
 };
 
 export default MenuContainer;
