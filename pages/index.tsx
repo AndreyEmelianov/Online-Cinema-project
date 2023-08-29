cf8a93a385e26fd8aace713f8b976ee7ae4a60b7
Andrey Emelianov <621077@mail.ru>
2023-08-29 18:35:29 +0300

    поменял глобальные стили, создал базовую структуру папок, начал реализацию layout и sidebar компонентов

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import Home from '@/components/screens/home/Home';
+
+import '../styles/globals.scss';
+
+export default function HomePage() {
+	return <Home />;
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,10 @@ const plugin = require('tailwindcss/plugin');
 const primary = '#e30b13';
 
 const config: Config = {
-	content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './app/components/**/*.{js,ts,jsx,tsx,mdx}'],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/components/**/*.{js,ts,jsx,tsx,mdx}',
+	],
 	theme: {
 		colors: {
 			primary,
@@ -40,7 +43,8 @@ const config: Config = {
 			},
 			backgroundImage: {
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
 			},
 			transitionTimingFunction: {
 				DEFAULT: 'ease-in-out',
