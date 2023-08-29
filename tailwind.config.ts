3641302535ff3fa415f340b714db6ac9ebe83774
Andrey Emelianov <621077@mail.ru>
2023-08-29 14:50:08 +0300

    добавил цвета, расширил размерз шрифта, транзишины, З индексы, добавил кейфреймы и анимацию

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,11 +12,69 @@ const config: Config = {
 			primary,
 			black: colors.black,
 			white: colors.white,
+			transparent: colors.transparent,
+			yellow: {
+				700: '#f5c521',
+			},
+			gray: {
+				300: '#d9dae8',
+				500: '#999aa5',
+				600: '#66676e',
+				700: '#39393f',
+				800: '#242529',
+				900: '#191b1f',
+				950: '#101215',
+			},
 		},
 		extend: {
+			spacing: {
+				0.5: '0.12rem',
+				layout: '2.75rem',
+			},
+			fontSize: {
+				'2lg': '1.38rem',
+			},
+			borderRadius: {
+				image: '0.5rem',
+				layout: '0.8rem',
+			},
 			backgroundImage: {
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
 				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			transitionTimingFunction: {
+				DEFAULT: 'ease-in-out',
+			},
+			transitionDuration: {
+				DEFAULT: '200ms',
+			},
+			zIndex: {
+				1: '1',
+				2: '2',
+				3: '3',
+			},
+			keyframes: {
+				fade: {
+					from: { opacity: '0' },
+					to: { opacity: '1' },
+				},
+				scaleIn: {
+					'0%': {
+						opacity: '0',
+						transform: 'scale(0.9)',
+					},
+					'50%': {
+						opacity: '0.3',
+					},
+					'100%': {
+						opacity: '1',
+						transform: 'scale(1)',
+					},
+				},
+			},
+			animation: {
+				fade: 'fade .5s ease-in-out',
+				scaleIn: 'scaleIn .35s ease-in-out',
 			},
 		},
 	},
